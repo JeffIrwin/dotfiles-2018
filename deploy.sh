fe926f8f48541e93166b253f5b01d5a79dfe4e28
Jeff Irwin <JeffIrwin@users.noreply.github.com>
2018-11-15 14:08:19 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,11 +4,15 @@ cp ~/.bash_profile ~/dotfile-backup
 cp ~/.bashrc ~/dotfile-backup
 cp ~/.inputrc ~/dotfile-backup
 cp ~/.vimrc ~/dotfile-backup
+cp ~/.gitignore ~/dotfile-backup
 
 cp .bash_profile ~
 cp .bashrc ~
 cp .inputrc ~
 cp .vimrc ~
+cp .gitignore ~
 
 mkdir ~/.vim
 cp supertab.vmb ~/.vim
+
+git config --global core.excludesfile ~/.gitignore
